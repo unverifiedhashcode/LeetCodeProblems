aabd9ee1c7e17f76d72522041f1cd9c94aeb7e64
uvhc <petro_zp@proton.me>
2026-02-10 22:20:18 -0700

    2026-1-24

---
+++ SQL/1211. Queries Quality and Percentage.sql
@@ -0,0 +1,6 @@
+select
+    query_name,
+    round(avg(rating / position),2) as quality,
+    Round(avg(case when rating < 3 then 1 else 0 end) * 100,2) as poor_query_percentage 
+    from Queries
+    group by query_name
