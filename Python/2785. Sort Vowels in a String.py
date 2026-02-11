db0037781cebaea32eb003d1c439b4ecd14c3afb
uvhc <petro_zp@proton.me>
2026-02-10 22:20:18 -0700

    $(date +%Y %m %d)

---
+++ Python/2785. Sort Vowels in a String.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        vowels = ['a', 'e', 'i', 'o', 'u','A', 'E', 'I', 'O', 'U',]
+        vowelsSpotted = []
+        vowelLocs = []
+        for i in range(len(s)):
+            if s[i] in vowels:
+                vowelsSpotted.append(s[i])
+                vowelLocs.append(i)
+        vowelsSpotted.sort()
+        #print(vowelsSpotted)
+        sList = list(s)
+        for i in range(len(vowelLocs)):
+            sList[vowelLocs[i]] = vowelsSpotted[i]
+        return "".join(sList)
