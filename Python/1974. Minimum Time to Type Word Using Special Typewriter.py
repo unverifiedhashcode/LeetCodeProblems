b13c69bc472c0a92122dfba1e8672d4a059382c7
uvhc <54560001+unverifiedhashcode@users.noreply.github.com>
2026-04-14 11:03:24 -0600

    2026-04-14

--- Python/1974. Minimum Time to Type Word Using Special Typewriter.py
+++ Python/1974. Minimum Time to Type Word Using Special Typewriter.py
@@ -4,7 +4,6 @@ class Solution:
         
         wheelLen = 26
         mid = wheelLen / 2
-        wheelPos = 0
 
         time = 0
 
@@ -12,27 +11,11 @@ class Solution:
             currLetter = word[wordPos]
             nextLetter = word[wordPos + 1]
 
-            dist  =  ord(nextLetter) - ord(currLetter)
-            print('dist from',currLetter,'to',nextLetter,'is',dist)
-            if dist >= 0: #dist pos
-                if dist > mid:
-                    time += (wheelLen - dist)
-                    print('PR')
-                else:
-                    time += dist
-                    print('PL')
-                time +=1
-                
-            else: #dist neg
-                dist*=-1
-                if dist < mid:
-                    time += dist
-                    print('NL')
-                else:
-                    time += (wheelLen - dist)
-                    print('NR')
-                time +=1
-            print('nt:',time)
+            dist  =  abs(ord(nextLetter) - ord(currLetter))
+            #print('dist from',currLetter,'to',nextLetter,'is',dist)
+            time += min(dist,wheelLen-dist) +1
+            
+            #print('nt:',time)
             #no need to reassign currLetter, it's taken care of automatically with the loop iteration
 
         return(time) 
